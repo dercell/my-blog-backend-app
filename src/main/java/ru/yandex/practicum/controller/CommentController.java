9869a2ce7b325c4ac2bd4c49c8b0436a5e462377
Sergey.Kolotevsergey.kolotev@glowbyteconsulting.com <you@example.com>
2026-04-05 00:55:31 +0300

    feat:  - add comment findAllByPostId

--- src/main/java/ru/yandex/practicum/controller/CommentController.java
+++ src/main/java/ru/yandex/practicum/controller/CommentController.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.model.Comment;
+import ru.yandex.practicum.service.CommentService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts/{id}/comments")
+@AllArgsConstructor
 public class CommentController {
 
+    private final CommentService commentService;
 
+    @GetMapping
+    public List<Comment> getAll(@PathVariable("id") Long postId){
+        return commentService.findAllByPostId(postId);
+    }
 
 }
--- src/main/java/ru/yandex/practicum/service/CommentService.java
+++ src/main/java/ru/yandex/practicum/service/CommentService.java
@@ -2,8 +2,19 @@ package ru.yandex.practicum.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.dao.CommentDao;
+import ru.yandex.practicum.model.Comment;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class CommentService {
+
+    private final CommentDao commentDao;
+
+    public List<Comment> findAllByPostId(Long postId){
+        return commentDao.findAllByPostId(postId);
+    }
+
 }
