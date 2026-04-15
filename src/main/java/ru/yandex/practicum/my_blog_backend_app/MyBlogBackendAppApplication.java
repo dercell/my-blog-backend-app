2655a5d7cf63afc198aaf016457322fc6f12d8a8
Sergey.Kolotevsergey.kolotev@glowbyteconsulting.com <you@example.com>
2026-04-15 18:52:58 +0300

    feat: init spring-boot project

---
+++ src/main/java/ru/yandex/practicum/my_blog_backend_app/MyBlogBackendAppApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.my_blog_backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/practicum/my_blog_backend_app/MyBlogBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.my_blog_backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
